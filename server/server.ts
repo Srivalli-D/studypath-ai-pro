63fd0e86043939e0343816ef979882b7993d6b09
Dasari Srivalli <srivallidasari1307@gmail.com>
2026-05-04 02:04:39 +0530

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -20,7 +20,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
