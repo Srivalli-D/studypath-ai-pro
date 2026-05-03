6ab50dbd3cd3ca08f25759332c8aae61787dd059
Dasari Srivalli <srivallidasari1307@gmail.com>
2026-05-04 01:46:23 +0530

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -9,7 +9,11 @@ import careerRoutes from "./routes/career.js";
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
   app.use(cors());
   app.use(express.json());
